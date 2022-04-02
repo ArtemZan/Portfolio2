78ea88f91472990d696a4b28843e61d6b324575f
ArtemZan <arttema9@gmail.com>
2022-04-02 08:51:48 +0300

    Replace background images from landing page with html and css

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,44 +2,14 @@ import { MutableRefObject, useEffect, useState } from "react";
 import { Button, Link } from "../../components/components";
 
 export default function Home(): JSX.Element {
-    const [mobileView, SetMobile] = useState(window.innerWidth < 600)
-
-    //Keep reference of all images so they are not deleted from memory
-    const preloadedImages = []
-
-    function OnResize(e: UIEvent) {
-        if (window.innerWidth < 600) {
-            SetMobile(true)
-        }
-        else {
-            SetMobile(false)
-        }
-    }
-
-    function PreloadImage(src: string) {
-        const image = new Image()
-        image.src = "images/" + src
-        image.className = "background"
-        image.alt = "background"
-        preloadedImages.push(image)
-    }
-
-    useEffect(() => {
-        PreloadImage("bg_mobile.png")
-        PreloadImage("bg_desktop.png")
-
-        window.addEventListener("resize", OnResize)
-
-        return () => window.removeEventListener("resize", OnResize)
-    }, [])
-
     return <div className="page home-page">
         <div className="header">
             <header>Artem<br />Zankovskiy</header>
             <h5>Programmer & designer</h5>
         </div>
         <div className="background">
-            <img src={mobileView ? "images/bg_mobile.png" : "images/bg_desktop.png"} alt="background" />
+            <div />
+            <div />
         </div>
         <div className="arrows">
             <div>
